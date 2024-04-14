d67a021f590c59a7efeb8983275006cf2ad74209
Vaidotas Jastrumskas <vaidotas.jastrumskas@gmail.com>
2024-04-14 13:52:03 +0300

    second commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+// here we are using CommonJS modules instead of ES modules
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-param-reassign": "off",
+  },
+};
