4f49237b5a43641ad572220750ae8cbd5dc873a8
Vaidotas Jastrumskas <vaidotas.jastrumskas@gmail.com>
2024-04-23 22:41:04 +0300

    Backup 10

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -3,12 +3,12 @@ import PiniaDisplay from '../components/PiniaDisplay.vue';
 import StatusDisplayContainer from '../components/StatusDisplayContainer.vue';
 import FoodsContainer from '../components/FoodsContainer.vue';
 import AddedFoodsContainer from '../components/AddedFoodsContainer.vue';
-import DaysBrowser from '../components/DaysBrowser.vue';
+import DatesNav from '../components/DatesNav.vue';
 </script>
 
 <template>
   <StatusDisplayContainer />
-  <DaysBrowser />
+  <DatesNav />
   <AddedFoodsContainer />
   <FoodsContainer />
   <PiniaDisplay />
